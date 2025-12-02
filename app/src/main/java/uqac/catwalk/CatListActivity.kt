977fa1511f502b05e921cc8750ad6b77e9fbea69
leaaah2 <rissel.lea@gmail.com>
2025-12-02 12:07:54 -0500

    merge good

--- app/src/main/java/uqac/catwalk/CatListActivity.kt
+++ app/src/main/java/uqac/catwalk/CatListActivity.kt
@@ -41,7 +41,6 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import androidx.core.view.WindowCompat.enableEdgeToEdge
 import uqac.catwalk.sauvegarde.AppDatabase
 import uqac.catwalk.ui.theme.CatwalkTheme
 
