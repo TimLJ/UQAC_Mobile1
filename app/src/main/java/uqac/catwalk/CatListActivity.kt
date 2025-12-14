2c1d546560ce9c72d1b3643eb0bbe7a2addd9c77
leaaah2 <rissel.lea@gmail.com>
2025-12-14 12:20:01 -0500

    couleur card

--- app/src/main/java/uqac/catwalk/CatListActivity.kt
+++ app/src/main/java/uqac/catwalk/CatListActivity.kt
@@ -25,6 +25,7 @@ import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -114,7 +115,12 @@ fun CatContent(modifier: Modifier = Modifier) {
                         horizontalArrangement = Arrangement.spacedBy(8.dp)
                     ) {
                         items(cats) { cat ->
-                            Card {
+                            Card(
+                                modifier = Modifier.fillMaxWidth(),
+                                colors = CardDefaults.cardColors(
+                                    containerColor = MaterialTheme.colorScheme.primaryContainer
+                                ),
+                            ) {
                                 Column(
                                     modifier = Modifier
                                         .padding(bottom = 10.dp)
